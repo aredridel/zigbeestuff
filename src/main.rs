ec75f3b25343c4b3df44ae8b561a9a835a2c129e
Aria Stewart <aredridel@dinhe.net>
2021-04-26 23:28:57 -0400

    It is alive

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
+use eyre::Result;
 use rumqttc::{AsyncClient, Event, Incoming, MqttOptions, QoS};
 use serde::Deserialize;
-use std::time::Duration;
-use eyre::Result;
+use sonor::Speaker;
+use std::collections::HashMap;
 use std::fs;
+use std::str;
+use std::time::Duration;
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
     let config: Config = toml::from_str(&fs::read_to_string("config.toml")?)?;
     let mut mqttoptions = MqttOptions::new("rumqtt-async", "homeassistant.local", 1883);
     mqttoptions.set_keep_alive(5);
-    mqttoptions.set_clean_session(true);
-    mqttoptions.set_credentials(config.username, config.password);
+    mqttoptions.set_clean_session(false);
+    mqttoptions.set_credentials(config.username.clone(), config.password.clone());
     mqttoptions.set_connection_timeout(30);
     mqttoptions.set_max_packet_size(65535, 65535);
 
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-    client
-        .subscribe("homeassistant/+/+/+/config", QoS::AtMostOnce)
-        .await?;
+    /* client
+    .subscribe("homeassistant/+/+/+/config", QoS::AtMostOnce)
+    .await?;*/
 
-    for bind in config.bind {
-        do_bind(&bind, &client).await?;
-    }
+    let mut app: App = App {
+        bindings: HashMap::new(),
+        client: client,
+    };
+
+    app.connect_bindings(config).await?;
 
     /*
     task::spawn(async move {
@@ -41,11 +47,8 @@ pub async fn main() -> Result<()> {
                 println!("Received = {:?}", notification);
                 if let Event::Incoming(Incoming::Publish(packet)) = notification {
                     println!("topic = {:?}", packet.topic);
-                    let v: serde_json::Result<HAConfig> = serde_json::from_slice(&packet.payload);
-                    match v {
-                        Ok(payload) => println!("payload = {:?}", payload),
-                        Err(_) => println!("payload = {:?}", packet.payload),
-                    }
+                    app.handle_payload(&packet.topic, str::from_utf8(&packet.payload).unwrap())
+                        .await?;
                 }
             }
             Err(e) => {
@@ -55,22 +58,66 @@ pub async fn main() -> Result<()> {
     }
 }
 
-async fn do_bind(bind: &BindConfig, client: &AsyncClient) -> Result<()> {
-    client
-        .subscribe(&bind.action_topic, QoS::AtMostOnce)
-        .await?;
-    let speaker = sonor::find(&bind.speaker, Duration::from_secs(2))
-        .await?
-        .expect("room exists");
+#[derive(Debug)]
+struct App {
+    bindings: HashMap<String, Vec<Binding>>,
+    client: AsyncClient,
+}
 
-    println!("The volume is currently at {}", speaker.volume().await?);
-
-    match speaker.track().await? {
-        Some(track_info) => println!("- Currently playing '{}", track_info.track()),
-        None => println!("- No track currently playing"),
+impl App {
+    async fn connect_bindings(&mut self, config: Config) -> Result<()> {
+        for bind in config.bind {
+            self.do_bind(bind).await?;
+        }
+        Ok(())
     }
 
-    Ok(())
+    async fn handle_payload(&mut self, topic: &str, payload: &str) -> Result<()> {
+        println!("payload = {:?}, topic = {}", payload, topic);
+        if let Some(bindings) = self.bindings.get(topic) {
+            for binding in bindings {
+                if payload == "brightness_move_up" {
+                    binding.speaker.set_volume_relative(1).await?;
+                } else if payload == "brightness_move_down" {
+                    binding.speaker.set_volume_relative(-1).await?;
+                } else if payload == "brightness_stop" {
+                }
+            }
+        }
+        Ok(())
+    }
+
+    async fn do_bind(&mut self, bind: BindConfig) -> Result<()> {
+        println!("topic = {}", bind.action_topic);
+
+        self.client
+            .subscribe(&bind.action_topic, QoS::AtMostOnce)
+            .await?;
+
+        let speaker = sonor::find(&bind.speaker, Duration::from_secs(2))
+            .await?
+            .expect("room exists");
+
+        println!("The volume is currently at {}", speaker.volume().await?);
+
+        match speaker.track().await? {
+            Some(track_info) => println!("Currently playing '{}", track_info.track()),
+            None => println!("No track currently playing"),
+        }
+
+        let binding = Binding {
+            speaker: speaker,
+            action_topic: bind.action_topic.clone(),
+        };
+        if let Some(vec) = self.bindings.get_mut(&bind.action_topic) {
+            vec.push(binding);
+        } else {
+            self.bindings
+                .insert(binding.action_topic.clone(), vec![binding]);
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -113,4 +160,10 @@ struct Config {
 struct BindConfig {
     action_topic: String,
     speaker: String,
+}
+
+#[derive(Debug)]
+struct Binding {
+    action_topic: String,
+    speaker: Speaker,
 }
